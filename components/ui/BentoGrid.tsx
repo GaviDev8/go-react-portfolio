37dba4e84574fc1483bb7d05da2d990033572aad
Gabriela <gabrielaaortiz8@gmail.com>
2024-06-22 21:36:22 -0700

    added globe and confetti to project cards

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,10 @@
+'use client'
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe"
+import { useState } from "react"
+import animationData from "@/data/confetti.json"
+import Lottie from "react-lottie"
 
 export const BentoGrid = ({
   className,
@@ -39,10 +44,11 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+    const [copied, setCopied] = useState(false)
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border-white/[0.1]",
         className
       )}
       style={{
@@ -86,9 +92,54 @@ export const BentoGridItem = ({
             </div>
             <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
           {title}
+            
         </div>
+          {id === 2 && <GlobeDemo />} 
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              {/* tech stack lists */}
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                {['HTML', 'Javascript' , 'MongoDB'].map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                 <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+            </div>
+            <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                {['CSS', 'SQL' , 'React.js'].map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                 <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+            </div>
         </div>
+          )}
+          {id === 6 && (
+            <div className="mt-5 relative">
+            <div className={`absolute -bottom-5 right-0`}>
+                <Lottie options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData: animationData,
+                    renderSettings: {
+                        preserveAspectRatio: 'xMidYMid slice',
+                    }
+                }} />
+            </div>
+        </div>
+          )}    
     </div>
+</div>
 </div>
   );
 };
