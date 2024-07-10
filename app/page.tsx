2b15c82f4c74895a947fc933d742537cb7d10b2e
Gabriela <gabrielaaortiz8@gmail.com>
2024-07-10 14:52:24 -0700

    adding assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects"
+import TechStack from "@/components/TechStack"
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
 import Image from "next/image";
@@ -14,6 +15,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <TechStack />
       </div>
     </main>
   );
---
+++ components/TechStack.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TechStack = () => {
+  return (
+<div className="py-20" id="projects">
+        <h1 className="heading">
+            My {' '}
+            <span className="text-purple">Tech Stack</span>
+        </h1>
+      <div className="flex flex-wrap items-center">      
+    </div>
+</div>
+  )
+}
+
+export default TechStack
