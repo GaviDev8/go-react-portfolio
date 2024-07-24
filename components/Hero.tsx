4a1ee9bb6e07207d8d2873b77b281ba9d68f2fa4
Gabriela <gabrielaaortiz8@gmail.com>
2024-07-24 15:50:54 -0700

    editing hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,15 +33,15 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+          Program Manager | Executive Operations | Software Developer | Plus other things
           </p>
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Hi! I&apos;m Gabriela, a Developer based in Los Angeles, CA."
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Gabriela, a Developer based in Los Angeles, CA.
+          The Quick Intro: problem-solving guru, cybersecurity student, coding enthusiast, executive admin, and passionate about blending big ideas with a good laugh. Oh, and I'm an organizing whiz - mastering projects and enjoying every detail!
           </p>
 
           <a href="#about">
